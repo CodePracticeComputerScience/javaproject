568ff58a99cd8fdd6fe71b1b1628507d81164846
Obi Jack <obijack59@gmail.com>
2018-01-30 21:38:41 -0600

    Partial code java

---
+++ src/javaproject/Mot2.java
@@ -0,0 +1,21 @@
+package javaproject;
+
+public class Mot2 {
+
+}
+
+
+//ArrayList<String> mot2(int n){
+//ArrayList<String> result = new ArrayList<String>();
+//if(n == 0) {
+//	 result.add("leaf");
+//	 return result;
+//}
+//ArrayList<String> nodes = mot2(n-1);
+//for(String node: nodes) {
+//	 result.add("unary(" + node + ")");
+//}
+//
+//}
+//}
+//}
