b25687e6a1608589a1c0c6ac1df2a645a0b5680e
Obi Jack <obijack59@gmail.com>
2018-01-30 22:15:55 -0600

    update mot2

--- src/javaproject/Mot2.java
+++ src/javaproject/Mot2.java
@@ -1,6 +1,44 @@
 package javaproject;
 
+import java.util.ArrayList;
+
 public class Mot2 {
+	ArrayList<String>  generator(int n) {
+		//TODO 
+		ArrayList<String> result = new ArrayList<String>();
+		if(n==0) {
+			result.add("leaf");
+			return result;
+		}
+		
+		ArrayList<String> nodes = generator(n-1);
+		for(String node: nodes) {
+			result.add("unary(" + node + ")");
+		}
+		for(int k = 0;k < n-2;k++) {
+			ArrayList<String> nodes1 = generator(k);
+			   for(String node1: nodes1) {
+				ArrayList<String> nodes2 =generator(n-2-k);
+				for(String node2: nodes2) {
+					result.add("binary(" + node1 + "," + node2 + ")");
+		//return result;
+				}
+			}
+		}
+		return nodes;
+		
+	}
+	
+	
+	
+	
+	
+	public static void main() {
+		Mot2 m = new Mot2();
+		for(int i = 0;i<6;i++)	{
+				m.generator(6);
+		}
+	}
 
 }
 
